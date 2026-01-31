8a09055b03c496090d3acb496f9d1bbc9657f191
linyingyang <yanglinying1024@gmail.com>
2026-01-31 16:28:08 +0000

    update pkg version to 0.1.3

--- src/frengression/__init__.py
+++ src/frengression/__init__.py
@@ -2,4 +2,4 @@ from .frengression import Frengression, FrengressionSeq, FrengressionSurv
 
 __all__ = ["Frengression", "FrengressionSeq", "FrengressionSurv"]
 
-__version__ = "0.1.0"
+__version__ = "0.1.3"
